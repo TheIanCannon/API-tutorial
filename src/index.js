00865681dcc219623c285fee199365e4b41236e4
Ian Cannon <ian.cannon@protonmail.com>
2022-03-17 12:50:26 -0700

    Update addItem function, remove unneeded definitions for localstorage

--- src/index.js
+++ src/index.js
@@ -25,6 +25,14 @@ api.listen(3000, () => {
 });
 
 api.post('/add', (req, res)=>{
-    console.log(req.body);
-    res.send("It's working!");
+    connection.query('INSERT INTO tasks (description) VALUES (?)', [req.body.item], (error, results) => {
+        if (error) return res.json({ error: error});
+        connection.query('SELECT LAST_INSERT_ID() FROM tasks', (error, results) => {
+            if (error) return res.json({ error: error});
+            res.json({
+                id: results[0]['LAST_INSERT_ID()'],
+                description: req.body.item
+            });
+        });
+    });
 });
